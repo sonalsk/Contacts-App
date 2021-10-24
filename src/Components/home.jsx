5710cd2ac6a2e24cf54108fd3e078fccf148e565
sonalsk <sonalkushwaha039@gmail.com>
2021-10-24 15:32:39 +0530

    Change UI of home page

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,6 +1,19 @@
+import { Container, Grid } from '@mui/material';
+import '../CSS/home.css'
+import bg from '../Assets/Images/bg.png';
+
 const Home = () => {
     return (
-        <p>Hi from Homepage</p>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={6} md={4}>
+                    <img className="home-bg" src={bg} alt="home-bg"></img>
+                </Grid>
+                <Grid item xs={6} md={8}>
+                    <p className="home-text">Your Personal <br /> Contact Diary</p>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,27 +1,20 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import { Container } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles({
-    tabs: {
-        color: '#FFFFFF',
-        textDecoration: 'none',
-        marginRight: '20px',
-        fontSize: 20
-    }
-});
+import '../CSS/home.css'
 
 const NavBar = () => {
-    const classes = useStyles();
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <NavLink className={classes.tabs} to="./" exact> Home </NavLink>
-                <NavLink className={classes.tabs} to="/all" exact> All Contacts </NavLink>
-                <NavLink className={classes.tabs} to="/add" exact> Add Contact </NavLink>
-            </Toolbar>
-        </AppBar>
+        <Container>
+            <AppBar className="head" position="static">
+                <Toolbar>
+                    <NavLink className="tabs" to="./" exact> Home </NavLink>
+                    <NavLink className="tabs" to="/all" exact> All Contacts </NavLink>
+                    <NavLink className="tabs" to="/add" exact> New Contact </NavLink>
+                </Toolbar>
+            </AppBar>
+        </Container>
     )
 }
 
