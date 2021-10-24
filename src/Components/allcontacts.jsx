330a49a96f80cfee1c94eac17559df1ff2bb0a54
sonalsk <sonalkushwaha039@gmail.com>
2021-10-24 16:08:53 +0530

    Change UI of all contacts page

--- src/Components/allcontacts.jsx
+++ src/Components/allcontacts.jsx
@@ -3,21 +3,20 @@ import { useEffect, useState } from "react";
 import { deleteUser, getUsers } from "../Service/api";
 import { makeStyles } from '@mui/styles';
 import { Link } from "react-router-dom";
+import { Container } from "@mui/material";
 
 const useStyles = makeStyles({
-    table: {
-        width: "90%",
-        margin: "50px 0 0 50px"
-    },
     thead: {
         '& > *': {
-            fontSize: 20,
-            color: '#FFFFFF'
+            fontSize: 25,
+            background: '#ff5757',
+            color: '#FFFFFF',
         }
     },
     row: {
         '& > *': {
-            fontSize: 18
+            color: '#FFFFFF',
+            fontSize: 20
         }
     }
 });
@@ -42,37 +41,38 @@ const AllContacts = () => {
     }
 
     return (
-        <Table className={classes.table}>
-            <TableHead>
-                <TableRow className={classes.thead}>
-                    <TableCell> ID </TableCell>
-                    <TableCell> Name </TableCell>
-                    <TableCell> Username </TableCell>
-                    <TableCell> Email </TableCell>
-                    <TableCell> Phone </TableCell>
-                    <TableCell> </TableCell>
-                </TableRow>
-            </TableHead>
+        <Container>
+            <Table>
+                <TableHead>
+                    <TableRow className={classes.thead}>
+                        <TableCell> ID </TableCell>
+                        <TableCell> Name </TableCell>
+                        <TableCell> Username </TableCell>
+                        <TableCell> Email </TableCell>
+                        <TableCell> Phone </TableCell>
+                        <TableCell> </TableCell>
+                    </TableRow>
+                </TableHead>
 
-            <TableBody>
-                {
-                    contacts.map(user => (
-                        <TableRow className={classes.row} key={user.id}>
-                            <TableCell> {user.id} </TableCell>
-                            <TableCell> {user.name} </TableCell>
-                            <TableCell> {user.username} </TableCell>
-                            <TableCell> {user.email} </TableCell>
-                            <TableCell> {user.phone} </TableCell>
-                            <TableCell> 
-                                <Button variant="contained" color="primary" style={{marginRight: 10}} component={Link} to={`/edit/${user.id}`}>Edit</Button>
-                                <Button variant="contained" color="secondary" onClick={() => deleteUserData(user.id)}>Delete</Button>
-                            </TableCell>
-                        </TableRow>
-                    ))
-                }
-            </TableBody>
-        </Table>
-
+                <TableBody>
+                    {
+                        contacts.map(user => (
+                            <TableRow className={classes.row} key={user.id}>
+                                <TableCell> {user.id} </TableCell>
+                                <TableCell> {user.name} </TableCell>
+                                <TableCell> {user.username} </TableCell>
+                                <TableCell> {user.email} </TableCell>
+                                <TableCell> {user.phone} </TableCell>
+                                <TableCell> 
+                                    <Button variant="contained" color="primary" style={{marginRight: 10}} component={Link} to={`/edit/${user.id}`}>Edit</Button>
+                                    <Button variant="contained" color="secondary" onClick={() => deleteUserData(user.id)}>Delete</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </Container>
     )
 }
 
